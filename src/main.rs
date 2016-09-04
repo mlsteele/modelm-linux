9557b97505afdc7e08f661abbe126fcb1e17c3dd
Miles Steele <miles@milessteele.com>
2016-09-04 10:41:17 -0400

    it works

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
 extern crate ears;
+extern crate rand;
 
 mod sound;
 mod keys;
 
+use std::io;
+use std::thread;
+use rand::Rng;
+use rand::distributions::{Range, IndependentSample};
+use ears::{Sound, AudioController};
+use keys::KeyMotion;
+
 fn main() {
+    match progn() {
+        Ok(_) => (),
+        Err(err) => println!("Fatal: {}", err),
+    }
+}
+
+fn progn() -> Result<(), Box<std::error::Error>> {
     ears::init();
 
     println!("Hello, world!");
 
-    sound::main();
+    let mut sounds = try!(sound::load_dir("resources/modelm"));
+    if sounds.len() < 1 {
+        return try!(Err(io::Error::new(io::ErrorKind::Other, "no sounds loaded")));
+    }
 
     let keyboard = keys::Keyboard::new();
     keyboard.start();
@@ -16,5 +34,11 @@ fn main() {
     loop {
         let ke = keyboard.rx.recv().unwrap();
         println!("{:?}", ke);
+        if let KeyMotion::Press = ke.motion {
+            let range = Range::new(0, sounds.len());
+            let i = range.ind_sample(&mut rand::thread_rng());
+            let s = &mut sounds[i];
+            s.play();
+        }
     }
 }
--- src/sound.rs
+++ src/sound.rs
@@ -1,7 +1,31 @@
+use std::vec;
+use std::fs;
+use std::io;
+
 use ears::{Sound, AudioController};
 
+pub fn load_dir(path: &str) -> Result<Vec<Sound>, io::Error> {
+    let mut v: Vec<Sound> = Vec::new();
+    for entry in try!(fs::read_dir(path)) {
+        let entry = try!(entry);
+        let path = entry.path();
+        let path = match path.to_str() {
+            Some(s) => s,
+            None => return Err(io::Error::new(io::ErrorKind::Other, "bad path")),
+        };
+        if path.ends_with(".wav") {
+            if let Some(s) = Sound::new(path) {
+                v.push(s);
+            } else {
+                return Err(io::Error::new(io::ErrorKind::Other, "Could not load sound"));
+            }
+        }
+    }
+    return Ok(v);
+}
+
 pub fn main() {
-    let path = "/home/miles/code/vendor/modelm-linux/resources/modelm/1_.wav";
+    let path = "/home/miles/code/modelm-linux/resources/modelm/1_.wav";
     println!("Loading: {}", path);
     if let Some(mut s) = Sound::new(path) {
         println!("Loaded sound");
